93b232dbd610b447428c470efcc1fc1b1c9a867e
erica-joseph <84351866+erica-joseph@users.noreply.github.com>
2026-01-17 18:18:45 -0500

    Ahh

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {  HashRouter,  Routes, Route } from 'react-router-dom';
 
 import Home from './Pages/Home/Home.jsx';
 import About from './Pages/Home/About.jsx';
